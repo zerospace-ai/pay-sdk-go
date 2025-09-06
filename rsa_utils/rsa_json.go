9cfc02212a0bd740339057bce27d9e4b5d4c84b3
xtomars2048 <xtomars@zerospace.ai>
2025-09-06 18:06:07 +0800

    fix: sign problem;

--- rsa_utils/rsa_json.go
+++ rsa_utils/rsa_json.go
@@ -20,57 +20,56 @@ func ToStringMap(jStr []byte) map[string]string {
 }
 
 func valueAsString(element json.RawMessage) string {
-	var asString string
+	// Trying to parse as nil (JSON null)
+	if string(element) == "null" {
+		return ""
+	}
 
+	// Trying to parse as bool
+	var asBool bool
+	if err := json.Unmarshal(element, &asBool); err == nil {
+		return strconv.FormatBool(asBool)
+	}
+
+	// Trying to parse as Number
+	var asNumber json.Number
+	if err := json.Unmarshal(element, &asNumber); err == nil {
+		return asNumber.String()
+	}
+
+	// Trying to parse as string
+	var asString string
 	if err := json.Unmarshal(element, &asString); err == nil {
 		return asString
 	}
 
+	// Trying to parse as an object
 	var asObject map[string]json.RawMessage
 	if err := json.Unmarshal(element, &asObject); err == nil {
-		sortedKeys := make([]string, 0, len(asObject))
-		for key := range asObject {
-			sortedKeys = append(sortedKeys, key)
+		keys := make([]string, 0, len(asObject))
+		for k := range asObject {
+			keys = append(keys, k)
 		}
-		sort.Slice(sortedKeys, func(i, j int) bool {
-			return strings.ToLower(sortedKeys[i]) < strings.ToLower(sortedKeys[j])
+		sort.Slice(keys, func(i, j int) bool {
+			return strings.ToLower(keys[i]) < strings.ToLower(keys[j])
 		})
 
 		var buffer bytes.Buffer
-		for _, key := range sortedKeys {
-			buffer.WriteString(valueAsString(asObject[key]))
+		for _, k := range keys {
+			buffer.WriteString(valueAsString(asObject[k]))
 		}
 		return buffer.String()
 	}
 
+	// Trying to parse as an array
 	var asArray []json.RawMessage
 	if err := json.Unmarshal(element, &asArray); err == nil {
-		sortedKeys := make(map[string]string)
-		for _, item := range asArray {
-			var arrayObject map[string]json.RawMessage
-			if err := json.Unmarshal(item, &arrayObject); err == nil {
-				for key, value := range arrayObject {
-					sortedKeys[key] = valueAsString(value)
-				}
-			}
-		}
-
 		var buffer bytes.Buffer
-		keys := make([]string, 0, len(sortedKeys))
-		for key := range sortedKeys {
-			keys = append(keys, key)
-		}
-		sort.Strings(keys)
-
-		for _, key := range keys {
-			buffer.WriteString(sortedKeys[key])
+		for _, item := range asArray {
+			buffer.WriteString(valueAsString(item))
 		}
 		return buffer.String()
 	}
 
-	var asInt int64
-	if err := json.Unmarshal(element, &asInt); err == nil {
-		return strconv.FormatInt(asInt, 10)
-	}
 	return ""
 }
